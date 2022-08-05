bdb419774c94391512c86ff254515bf690c91066
rahul_chhetri <immarshallchhetri@gmail.com>
2022-08-05 12:37:45 +1000

    Handle progress and error

--- app/src/main/java/com/rahul/newsdemo/extensions/Common.kt
+++ app/src/main/java/com/rahul/newsdemo/extensions/Common.kt
@@ -1,8 +1,23 @@
 package com.rahul.newsdemo.extensions
 
+import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 
 fun ImageView.loadImage(url: String?) {
     Glide.with(context).load(url).into(this)
+}
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun Fragment.showToast(message: String, duration: Int = Toast.LENGTH_LONG) {
+    Toast.makeText(requireContext(), message, duration).show()
 }
--- app/src/main/java/com/rahul/newsdemo/ui/news/list/NewsListFragment.kt
+++ app/src/main/java/com/rahul/newsdemo/ui/news/list/NewsListFragment.kt
@@ -7,8 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.rahul.newsdemo.R
 import com.rahul.newsdemo.data.local.EventObserver
 import com.rahul.newsdemo.databinding.FragmentNewsListBinding
+import com.rahul.newsdemo.extensions.gone
+import com.rahul.newsdemo.extensions.showToast
+import com.rahul.newsdemo.extensions.visible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,9 +35,14 @@ class NewsListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupRecycler()
         viewModel.fetchTopNewsHeadlines()
+        setupUI()
         startObserving()
+    }
+
+    private fun setupUI() {
+        binding.toolBar.setTitle(R.string.app_name)
+        setupRecycler()
     }
 
     private fun setupRecycler() {
@@ -44,12 +53,12 @@ class NewsListFragment : Fragment() {
     }
 
     private fun startObserving() {
-        viewModel.loading.observe(viewLifecycleOwner) {
-
+        viewModel.loading.observe(viewLifecycleOwner) { loading ->
+            if (loading) binding.progressBar.visible() else binding.progressBar.gone()
         }
 
         viewModel.error.observe(viewLifecycleOwner, EventObserver {
-
+            showToast(it.message ?: "Unknown error")
         })
 
         viewModel.news.observe(viewLifecycleOwner) {
