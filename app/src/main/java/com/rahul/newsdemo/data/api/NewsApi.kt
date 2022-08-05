ee8e37b8f0d40a7e906ea92d76d64f35dfda4b56
rahul_chhetri <immarshallchhetri@gmail.com>
2022-08-05 10:17:06 +1000

    Setup api service to fetch top headlines

--- app/src/main/java/com/rahul/newsdemo/data/api/NewsApi.kt
+++ app/src/main/java/com/rahul/newsdemo/data/api/NewsApi.kt
@@ -1,4 +1,10 @@
 package com.rahul.newsdemo.data.api
 
+import com.rahul.newsdemo.data.remote.TopHeadlineResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NewsApi {
+    @GET("top-headlines?sortBy=publishedAt")
+    suspend fun getTopHeadlines(@Query("country") country: String): TopHeadlineResponse
 }
