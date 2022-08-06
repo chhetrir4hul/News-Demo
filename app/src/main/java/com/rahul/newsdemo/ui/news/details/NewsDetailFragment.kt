f0bde1c075c9a4f427cd1a7ddcc77ce19421e311
rahul_chhetri <immarshallchhetri@gmail.com>
2022-08-06 15:41:29 +1000

    Show article in detail page

--- app/src/main/java/com/rahul/newsdemo/ui/news/details/NewsDetailFragment.kt
+++ app/src/main/java/com/rahul/newsdemo/ui/news/details/NewsDetailFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.rahul.newsdemo.data.local.Article
 import com.rahul.newsdemo.databinding.FragmentNewsDetailsBinding
+import com.rahul.newsdemo.extensions.formatNewsTime
+import com.rahul.newsdemo.extensions.loadImage
 
 class NewsDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentNewsDetailsBinding
+
+    private val args: NewsDetailFragmentArgs by navArgs()
+
+    private var article: Article? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +26,18 @@ class NewsDetailFragment : Fragment() {
     ): View {
         binding = FragmentNewsDetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        article = args.article
+
+        binding.ivImage.loadImage(article?.urlToImage)
+        binding.tvTitle.text = article?.title ?: "-"
+        binding.tvAuthor.text = article?.author ?: "-"
+        binding.tvPublishedDate.text =
+            article?.publishedAt?.formatNewsTime()
+        binding.tvContent.text = article?.content
     }
 }
